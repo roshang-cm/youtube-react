2c4516d1aa73192ec90e363904f9271ba0c63333
Roshan G <roshan.g@codingmart.com>
2020-01-01 21:59:57 +0530

    Updates

---
+++ precache-manifest.e64e3f89c4d61edcc7a2c2b985b2c099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb82d0923ed044a8851bb76d82025478",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "a38c73b833d4de6cbe53",
+    "url": "/youtube-app/static/css/main.84b917ad.chunk.css"
+  },
+  {
+    "revision": "8c6ae8a6446eb6acb0ff",
+    "url": "/youtube-app/static/js/2.c85c3414.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/youtube-app/static/js/2.c85c3414.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a38c73b833d4de6cbe53",
+    "url": "/youtube-app/static/js/main.dcd8391c.chunk.js"
+  },
+  {
+    "revision": "356b51d142fa0b6be6e1",
+    "url": "/youtube-app/static/js/runtime-main.fb31473b.js"
+  }
+]);
